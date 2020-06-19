7cea2a2ef28493201df41b90b6d422b7f993a28e
Michael Watts <Wattsjmichael@gmail.com>
2020-06-19 09:53:30 -0700

    add user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+//Business Logic
+function PizzaPie(vegetables, proteins, sizes){
+  this.vegetables = vegetables;
+  this.proteins = proteins;
+  this.sizes = sizes;
+  this.total = "";
+
+}
+
+PizzaPie.prototype.pizzaPiePrice = function() {
+  return this.vegetables + this.proteins + this.sizes;
+}
+
+//User Interface Logic
+$(document).ready(function() {
+  $("form#pizzaToppings").submit(function(event) {
+    event.preventDefault();
+    let vegetable = parseInt($("#vegetables").val());
+    let protein = parseInt($("#proteins").val());
+    let size = parseInt($("#sizes").val());
+    let newPizzaPie = new PizzaPie(vegetables, proteins, sizes)
+    $("#output").show();
+    $("#pizzaPrice").text(newPizzaPie.pizzaPiePrice());
+  });
+
+});

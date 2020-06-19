e44fc92e7190f96ceb732abd028b747c3885d519
Michael Watts <Wattsjmichael@gmail.com>
2020-06-19 14:44:37 -0700

    add checkbox scripts to scripts.js, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
-function PizzaPie(size, protein, vegetable){
+function PizzaPie(size, protein, vegetable, addOns){
   this.size = size;
   this.protein = protein;
   this.vegetable = vegetable;
+  this.addOns = addOns
   this.price = "";
 }
 
-PizzaPie.prototype.pizzaPieprice = function() {
-  return this.size + this.protein + this.vegetable;
+function calculateAddons(){
+  let el = document.getElementById("funToppings");
+  let products = el.getElementsByTagName("input");
+  let len = products.length;
+    for (var i=0; i<len; i++) {
+      if (products[i].type === 'checkbox') {
+        products[i].onclick = updateCost;
+      }
+
+    }
 }
+
+PizzaPie.prototype.pizzaPieprice = function() {
+  return this.size + this.protein + this.vegetable + this.addOns;
+}
+
 
 
 $(document).ready(function() {
@@ -16,7 +30,8 @@ $(document).ready(function() {
       let size = parseInt($("#size").val());
       let protein = parseInt($("#protein").val());
       let vegetable = parseInt($("#vegetable").val());
-      let newPizzaPie = new PizzaPie(size, protein, vegetable);
+      let addOns = parseInt($(["#addOns"].val()));
+      let newPizzaPie = new PizzaPie(size, protein, vegetable, addOns);
       $("#output").show();
       $("#pizzaPrice").text(newPizzaPie.pizzaPieprice());
 

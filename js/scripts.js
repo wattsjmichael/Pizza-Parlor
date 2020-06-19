a14f139a48d21f4fe804a05b4bc33a62b6c1c8f0
Michael Watts <Wattsjmichael@gmail.com>
2020-06-19 15:18:46 -0700

    remove checkbox array, returns functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,17 @@
-function PizzaPie(size, protein, vegetable, addOns){
+function PizzaPie(size, protein, vegetable){
   this.size = size;
   this.protein = protein;
   this.vegetable = vegetable;
-  this.addOns = addOns
   this.price = "";
 }
 
-function calculateAddons(){
-  let el = document.getElementById("funToppings");
-  let products = el.getElementsByTagName("input");
-  let len = products.length;
-    for (var i=0; i<len; i++) {
-      if (products[i].type === 'checkbox') {
-        products[i].onclick = updateCost;
-      }
 
-    }
-}
+
+
 
 PizzaPie.prototype.pizzaPieprice = function() {
-  return this.size + this.protein + this.vegetable + this.addOns;
+  return this.size + this.protein + this.vegetable;
 }
-
 
 
 $(document).ready(function() {
@@ -30,8 +20,7 @@ $(document).ready(function() {
       let size = parseInt($("#size").val());
       let protein = parseInt($("#protein").val());
       let vegetable = parseInt($("#vegetable").val());
-      let addOns = parseInt($(["#addOns"].val()));
-      let newPizzaPie = new PizzaPie(size, protein, vegetable, addOns);
+      let newPizzaPie = new PizzaPie(size, protein, vegetable);
       $("#output").show();
       $("#pizzaPrice").text(newPizzaPie.pizzaPieprice());
 
